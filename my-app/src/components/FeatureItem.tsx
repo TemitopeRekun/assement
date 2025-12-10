80123b803b7ab670da7f470a0245b95403545eca
Temitope <olalekanogunrekun@gmail.com>
2025-12-10 22:23:35 +0100

    initialising the repository

---
+++ my-app/src/components/FeatureItem.tsx
@@ -0,0 +1,24 @@
+type FeatureItemProps = {
+  icon: string;
+  title: string;
+  description: string;
+};
+
+export function FeatureItem({ icon, title, description }: Readonly<FeatureItemProps>) {
+  return (
+		<div className="flex items-start gap-4">
+			<img src={icon} alt="" className="w-15 h-15" />
+
+			<div>
+				<h3 className=" text-[#161723] text-[15px] font-semibold tracking-tight text-lg">
+					{title}
+				</h3>
+
+				<p className="text-[12px] leading-snug mt-1">
+					{description}
+				</p>
+			</div>
+		</div>
+  );
+}
+export default FeatureItem;
